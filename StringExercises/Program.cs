a4e3122d5677ac30d862927bd62ce212d13da174
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-06-03 11:56:27 -0400

    Changes to String Exercise

--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -6,7 +6,17 @@ namespace StringExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strings!");
+            string phrase = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+            string[] words = phrase.Split(", ");
+
+            foreach (var word in words)
+
+
+            {
+                //Console.WriteLine($"<{word}>");
+                //Console.WriteLine(string.Join(" ", words));
+            }
+            Console.WriteLine(string.Join(" ", words));
         }
     }
 }
