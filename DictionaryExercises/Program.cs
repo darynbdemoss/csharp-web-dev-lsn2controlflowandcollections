3caec73ad15485536d979f7a90c3736317b9b522
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-06-03 17:26:11 -0400

    Changes to Dictionary Exercise

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DictionaryExercises
 {
@@ -6,7 +7,33 @@ namespace DictionaryExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, string> students = new Dictionary<int, string>();
+
+            string newStudent;
+
+            Console.WriteLine("Enter your students names and IDs(or Enter to Finish) :");
+
+            do
+            {
+                Console.Write("Student name: ");
+                newStudent = Console.ReadLine();
+
+                if (newStudent != "")
+                {
+                    Console.WriteLine("ID Number: ");
+                    int newID = Int32.Parse(Console.ReadLine());
+
+                    students.Add(newID, newStudent);
+                }
+
+            } while (newStudent != "");
+
+            Console.WriteLine("\nThe Class List: ");
+
+            foreach (KeyValuePair<int, string> student in students)
+            {
+                Console.WriteLine($"{student.Value}'s ID: {student.Key}");
+            }
         }
     }
 }
