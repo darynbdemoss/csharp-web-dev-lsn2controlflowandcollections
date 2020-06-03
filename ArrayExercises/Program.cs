5c69d4df09a72431d5ce36d3032be954b2b10b71
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-06-03 11:24:56 -0400

    Changes to array exercise

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -6,7 +6,15 @@ namespace ArrayExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arrays!");
+            int[] arr = { 1, 1, 2, 3, 5, 8 };
+            foreach (var item in arr)         
+
+
+            {
+                if (item % 2 != 0)
+                    Console.Write(item);               
+            }
+            
         }
     }
 }
